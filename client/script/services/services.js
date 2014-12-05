a602d803cd41c56ad1566f4bdfecfaa480bd87ac
Kingsten Banh <kingstenbanh@gmail.com>
2014-12-05 14:41:22 -0800

    add search button to the page

--- client/script/services/services.js
+++ client/script/services/services.js
@@ -3,13 +3,13 @@
 angular.module('shouldIApp.services', [])
 .factory('AutoCompleteService', function ($http) {
   var getSource = function(){
-       names = ["john", "bill", "charlie", "robert",
+      var names = ["john", "bill", "charlie", "robert",
        "alban", "oscar", "marie", "celine", "brad", "drew",
        "rebecca", "michel", "francis", "jean", "paul", "pierre",
        "nicolas", "alfred", "gerard", "louis", "albert", "edouard",
        "benoit", "guillaume", "nicolas", "joseph"];
        return names;
-    }
+    };
   return {
     getSource: getSource
   };
