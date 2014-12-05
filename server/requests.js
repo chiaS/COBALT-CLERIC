09efa88dec39e4e5531f67322dccf8c34ba258d2
Krystal Raphael <krystal.raphael@gmail.com>
2014-12-05 14:41:49 -0800

    Fixes error with creating schema

--- server/requests.js
+++ server/requests.js
@@ -4,14 +4,16 @@ var Result = require('./results.js');
 
 exports.saveResult = function (pitchforkResults) {
   
+  //creates new instance of result
   var newResult = new Result({
     artist: pitchforkResults.artist,
-	rating: pitchforkResults.rating,
-	timeStamp: pitchforkResults.timeStamp,
-	url: pitchforkResults.url
+	  rating: pitchforkResults.rating,
+	  timeStamp: pitchforkResults.timeStamp,
+	  url: pitchforkResults.url
   });
 
-  saveResult.save(function(err, newEntry){
+  //saves result to database
+  newResult.save(function(err, newEntry){
   	if(err){
   	  res.send(500);
   	}else{
