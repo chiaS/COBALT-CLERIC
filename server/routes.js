40278d41f3bac58a22d775f7dc7451c7eb094fc8
Chia Hsin <chiahsin@Chias-MacBook-Air.local>
2014-12-05 16:30:26 -0800

    solved conflict of autoComplete.html

--- server/routes.js
+++ server/routes.js
@@ -8,6 +8,9 @@ var Result = require('./results.js');
 var requests = require('./requests.js');
 
 var router = express.Router();
+var fs = require('fs');
+
+
 
 router.use(function(req, res, next) {
   // do logging
@@ -20,6 +23,22 @@ router.get('/', function(req, res) {
   //res.json({ message: 'hooray! welcome to our api!' }); 
   res.sendFile(path.join(__dirname, '../client/index.html'));
 });
+
+
+// about page route (http://localhost:8080/about)
+router.get('/about', function(req, res) {
+  res.send('im the about page!');
+});
+
+router.route('/artists')
+
+  .get(function(req, res) {
+      var artistsFile = path.join(__dirname, '../artist.json');
+      fs.readFile(artistsFile, function(err, data){
+        console.log(JSON.parse(data));
+        res.send(JSON.parse(data));
+      });  
+  });
 
 router.route('/artists/:artist_id')
 
