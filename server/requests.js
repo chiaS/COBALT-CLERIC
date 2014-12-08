182019139b2a36790cfceaa68f392797c1578db1
Chia Hsin <chiahsin@Chias-MacBook-Air.local>
2014-12-08 10:22:59 -0800

    resovled conflict on requests.js

--- server/requests.js
+++ server/requests.js
@@ -1,16 +1,19 @@
 'use strict';
 
 var Result = require('./results.js');
-
+var strip = require('./strip.js');
 var saveResult = function (data, callback) {
   //creates new instance of result
+
   var artist = data.artistName.toLowerCase();
   var newResult = new Result({
-    artistName: artist,
+
+    artistName: strip(artist),
 	  url: data.url,
     timestamp: data.timestamp,
     rating: data.rating
   });
+  console.log(newResult);
 
   //saves result to database
   newResult.save(callback);
