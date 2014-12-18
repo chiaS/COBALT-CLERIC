11d63594681c724e1bb32ad4deef85bf72a03439
Chia <pretend15@gmail.com>
2014-12-17 22:25:54 -0800

    move server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 
 var express = require('express');    // call express
 var app = express();
-var router = require('./server/routes.js');
+var router = require('./routes.js');
 var path = require('path');
-var connection = require('./server/connection');
+var connection = require('./connection');
 
 var port = process.env.PORT || 8080;
 
