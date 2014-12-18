72c4f3751d670f7b3bc68dec75dd54734c7c91e6
Chia <pretend15@gmail.com>
2014-12-17 22:50:30 -0800

    modify static path

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 
 var express = require('express');    // call express
 var app = express();
-var router = require('./routes.js');
+var router = require('./server/routes.js');
 var path = require('path');
-var connection = require('./connection');
+var connection = require('./server/connection');
 
 var port = process.env.PORT || 8080;
 
 app.listen(port);
 console.log('Magic happens on port ' + port);
-app.use(express.static(path.join(__dirname, '../client')));
+app.use(express.static(path.join(__dirname, 'client')));
 
 // apply the routes to our application
 app.use('/api', router);
